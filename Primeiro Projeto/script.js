84dbc98340e538e6091a8fd38df1310a4d6dd562
Guilherme Aparecido Andrade Pereira <81770786+Guizeus30@users.noreply.github.com>
2023-05-12 11:14:04 -0300

    Front-end
    Primeiro Projeto.

---
+++ Primeiro Projeto/script.js
@@ -0,0 +1,49 @@
+function Responder1() {
+  var R1 = document.querySelector('#resposta1')
+  if (R1 == 1809) {
+    alert("Você acertou!")
+  } else {
+    alert("Você errou! Resposta era 1809")
+  }
+  R1.value = ""
+}
+function Responder2() {
+  var R2 = document.querySelector('#resposta2')
+  var Resposta2 = R2.value
+  if (Resposta2 == 13){
+    alert("Você acertou!")
+  } else {
+    alert("Você errou! Resposta era 13")
+  }
+  R2.value = ""
+}
+function Responder3() {
+  var R3 = document.querySelector('#resposta3')
+  var Resposta3 = R3.value
+  if (Resposta3 == 230){
+    alert("Você acertou!")
+  } else {
+    alert("Você errou! Resposta era 230")
+  }
+  R3.value = ""
+}
+function Responder4() {
+  var R4 = document.querySelector('#resposta4')
+  var Resposta4 = R4.value
+  if (Resposta4 == 22){
+    alert("Você acertou!")
+  } else {
+    alert("Você errou! Resposta era 22")
+  }
+  R4.value = ""
+}
+function Responder5() {
+  var R5 = document.querySelector('#resposta5')
+  var Resposta5 = R5.value
+  if (Resposta5 == 4){
+    alert("Você acertou!")
+  } else {
+    alert("Você errou! Resposta era 4")
+  }
+  R5.value = ""
+}
